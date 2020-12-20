d0bed2fb7a516fdd5f1fbee661155388f3ec7a64
Collins Muiruri <wanyekicollins@gmail.com>
2020-12-21 01:16:45 +0300

    Add cart project

---
+++ 23/cart/cart.js
@@ -0,0 +1,28 @@
+var socialFloat = document.querySelector("#rights");
+var footer = document.querySelector("#footers");
+
+function checkOffset() {
+    function getRectTop(el) {
+        var rect = el.getBoundingClientRect();
+        return rect.top;
+    }
+
+    if (
+        getRectTop(socialFloat) +
+        document.body.scrollTop +
+        socialFloat.offsetHeight >=
+        getRectTop(footer) + document.body.scrollTop - 10
+    )
+        socialFloat.style.position = "absolute";
+    if (
+        document.body.scrollTop + window.innerHeight <
+        getRectTop(footer) + document.body.scrollTop
+    )
+        socialFloat.style.position = "fixed"; // restore when you scroll up
+
+    socialFloat.innerHTML = document.body.scrollTop + window.innerHeight;
+}
+
+document.addEventListener("scroll", function() {
+    checkOffset();
+});
